5ed43616048b986ef04c3bddbe9d79189d42b7e6
rorensu2236 <rorensu2236@gmail.com>
2023-09-04 10:15:37 +0900

    feat: CSS js html を適切に再配置

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,13 +1,11 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import Script from 'next/script';
 import Layout from '/components/layout';
 
 export default function FirstPost() {
   return (
     <Layout>
-      <Head><title>First Post</title></Head>
-      <h1>First Post</h1>
+      <Head><title>rorensu2236さんのツイート</title></Head>
+      <h1>ツイート一覧</h1>
     </Layout>
   );
 }
