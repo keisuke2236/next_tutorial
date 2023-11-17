405f8df0d76edb958ed19d5fcd9bc12fe1282b45
rorensu2236 <rorensu2236@gmail.com>
2023-11-17 09:56:37 +0900

    refactor: fix eslit first file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,37 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+export default {
+  env: {
+    browser: true,
+    es2021: true
   },
-  "extends": [
-    "standard-with-typescript",
-    "plugin:react/recommended"
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended'
   ],
-  "overrides": [
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
+      files: [
+        '.eslintrc.{js,cjs}'
       ],
-      "parserOptions": {
-        "sourceType": "script"
+      parserOptions: {
+        sourceType: 'script'
+      },
+      rules: {
+        '@typescript-eslint/*': 'off'
       }
     }
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
-  "plugins": [
-    "react"
+  plugins: [
+    'react'
   ],
-  "rules": {
-    "react/react-in-jsx-scope": "off"
+  rules: {
+    'react/react-in-jsx-scope': 'off'
   }
 }
